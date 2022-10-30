d2d3f85cde5c81cb95224fa042ccbaaf03e474e6
better-birth-control <ben@betterbirthcontrol.org>
2022-10-30 16:07:47 +0000

    Update from https://github.com/better-birth-control/better/commit/d62914bd7503e53809b40e323361917b344de442

--- resources/views/components/collapsible.blade.php
+++ resources/views/components/collapsible.blade.php
@@ -1,6 +1,6 @@
 <div class="better-collapsible {{ $last ? 'last' : '' }}" :class="{open: open }" x-data="{open: {{ $open ? 'true' : 'false' }}}" {{ $attributes }}>
     <div id="{{ $headerId }}" class="header" role="button" :aria-expanded="open ? 'true' : 'false'" aria-controls="{{ $contentId }}" tabindex="0" @click="open = !open; $dispatch('betterToggle', {open})">
-        {{ $header }}
+        <span class="flex-1">{{ $header }}</span>
         <x-better-icon name="nav-arrow-down" size="small"></x-better-icon>
     </div>
 
