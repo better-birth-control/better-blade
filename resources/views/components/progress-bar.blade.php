828e15653abf58351d787fb0ee52a15474e50ad4
better-birth-control <ben@betterbirthcontrol.org>
2023-01-23 13:00:14 +0000

    Update from https://github.com/better-birth-control/better/commit/d4bcc38212648d5fcd4e6cfd853cbf6add565ef8

--- resources/views/components/progress-bar.blade.php
+++ resources/views/components/progress-bar.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->merge(['class' => 'better-progress-bar']) }} value="{{ intval($current) }}" max="{{ intval($max) }}" x-data="dropdown" role="progressbar" :aria-valuenow="progress" aria-valuemin="0" aria-valuemax="100" x-cloak>
+<div {{ $attributes->merge(['class' => 'better-progress-bar']) }} value="{{ intval($current) }}" max="{{ intval($max) }}" x-data="betterprogressbar" role="progressbar" :aria-valuenow="progress" aria-valuemin="0" aria-valuemax="100" x-cloak>
     <div class="bar" :style="{width: barWidth}"></div>
     <div class="max" x-show="!percentageMode" x-text="max + ' ' + append"></div>
 </div>
